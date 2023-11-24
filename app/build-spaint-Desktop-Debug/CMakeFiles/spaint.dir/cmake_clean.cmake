e5b6a8c837d271f6f68dc9ae4f9c11b4e1820f1c
Mauro Garcia Coto <maurogarciacoto@gmail.com>
2023-11-23 22:07:31 -0300

    created empty qt project

---
+++ app/build-spaint-Desktop-Debug/CMakeFiles/spaint.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaint_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/spaint_autogen.dir/ParseCache.txt"
+  "spaint_autogen"
+  "CMakeFiles/spaint.dir/main.cpp.o"
+  "CMakeFiles/spaint.dir/main.cpp.o.d"
+  "CMakeFiles/spaint.dir/mainwindow.cpp.o"
+  "CMakeFiles/spaint.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/spaint.dir/spaint_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/spaint.dir/spaint_autogen/mocs_compilation.cpp.o.d"
+  "spaint"
+  "spaint.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spaint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ app/build-spaint-Desktop-Debug/CMakeFiles/spaint_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaint_autogen"
+  "spaint_autogen/include/ui_mainwindow.h"
+  "spaint_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/spaint_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
